3c3b4f79f6ebfd80738cdafa572b57335322cfae
kahosadi <kahosadi@gmail.com>
2017-06-09 16:04:22 +0700

    Index template

--- application/views/templates/default/front/body.php
+++ application/views/templates/default/front/body.php
@@ -4,75 +4,57 @@
 <section class="pt-4">
   <div class="row">
       <div class="col-lg-8 mb-4">
-        <div class="card text-center">
+        <!-- Nav tabs -->
+        <div class="card">
           <div class="card-header">
-            <ul class="nav nav-tabs card-header-tabs">
+            <ul class="nav nav-tabs card-header-tabs" role="tablist">
               <li class="nav-item">
-                <a class="nav-link active" href="#">Active</a>
+                <a class="nav-link active" data-toggle="tab" href="#home" role="tab">Buku</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" data-toggle="tab" href="#profile" role="tab">Jurnal</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
+                <a class="nav-link" data-toggle="tab" href="#messages" role="tab">Website</a>
               </li>
             </ul>
           </div>
           <div class="card-block">
-            <h4 class="card-title">Special title treatment</h4>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <!-- Tab panes -->
+            <div class="tab-content">
+              <div class="tab-pane active" id="home" role="tabpanel">
+                <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+              </div>
+              <div class="tab-pane" id="profile" role="tabpanel">
+                <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
+                </div>
+              <div class="tab-pane" id="messages" role="tabpanel">
+                <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.</p>
+                </div>
+            </div>
           </div>
         </div>
       </div>
       <div class="col-lg-4 mb-4">
           <div class="card h-100">
-              <h4 class="card-header">Card Title</h4>
               <div class="card-block">
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-              </div>
-              <div class="card-footer">
-                  <a href="#" class="btn btn-primary">Learn More</a>
+                  <div class="fb-page" data-href="https://www.facebook.com/libraryuksw" data-tabs="timeline" data-width="360" data-height="70" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
               </div>
           </div>
       </div>
   </div>
 </section>
+<hr>
 <section>
-  <div class="row">
-      <div class="col-lg-6 mb-4">
-          <div class="card h-100">
-              <h4 class="card-header">Card Title</h4>
-              <div class="card-block">
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-              </div>
-              <div class="card-footer">
-                  <a href="#" class="btn btn-primary">Learn More</a>
-              </div>
-          </div>
-      </div>
-      <div class="col-lg-6 mb-4">
-          <div class="card h-100">
-              <h4 class="card-header">Card Title</h4>
-              <div class="card-block">
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-              </div>
-              <div class="card-footer">
-                  <a href="#" class="btn btn-primary">Learn More</a>
-              </div>
-          </div>
-      </div>
-  </div>
-</section>
     <!-- Portfolio Section -->
-    <h2>Portfolio Heading</h2>
+    <h3>Layanan Perpustakaan</h3>
 
     <div class="row">
         <div class="col-lg-4 col-sm-6 portfolio-item">
             <div class="card h-100">
                 <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
                 <div class="card-block">
-                    <h4 class="card-title"><a href="#">Project One</a></h4>
+                    <h5 class="card-title"><a href="#">Peminjaman & Pengembalian</a></h5>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
                 </div>
             </div>
@@ -81,7 +63,7 @@
             <div class="card h-100">
                 <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
                 <div class="card-block">
-                    <h4 class="card-title"><a href="#">Project Two</a></h4>
+                    <h5 class="card-title"><a href="#">Laboratorium Komputer</a></h5>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
                 </div>
             </div>
@@ -90,45 +72,39 @@
             <div class="card h-100">
                 <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
                 <div class="card-block">
-                    <h4 class="card-title"><a href="#">Project Three</a></h4>
+                    <h5 class="card-title"><a href="#">Ruang Diskusi</a></h5>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!</p>
                 </div>
             </div>
         </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-            <div class="card h-100">
-                <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-block">
-                    <h4 class="card-title"><a href="#">Project Four</a></h4>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-            <div class="card h-100">
-                <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-block">
-                    <h4 class="card-title"><a href="#">Project Five</a></h4>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-            <div class="card h-100">
-                <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-block">
-                    <h4 class="card-title"><a href="#">Project Six</a></h4>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.</p>
-                </div>
-            </div>
-        </div>
     </div>
+  <div class="row">
+      <div class="col-lg-12 text-right"><a href="#" class="text-primary">Selengkapnya <span class="fa fa-plus-square fa-lg" aria-hidden="true"></span></a></div>
+  </div>
     <!-- /.row -->
-
-    <!-- Features Section -->
-    <div class="row">
-        <div class="col-lg-6">
-            <h2>Modern Business Features</h2>
+</section>
+<hr>
+<section>
+  <!-- Features Section -->
+      <div class="row">
+          <div class="col-lg-6">
+              <h3>Konten Elektronik</h3>
+              <p>The Modern Business template by Start Bootstrap includes:</p>
+              <ul>
+                  <li><strong>Bootstrap v4</strong>
+                  </li>
+                  <li>jQuery</li>
+                  <li>Font Awesome</li>
+                  <li>Working contact form with validation</li>
+                  <li>Unstyled page elements for easy customization</li>
+              </ul>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>
+              <div class="row">
+                  <div class="col-lg-12 text-right"><a href="#" class="text-primary">Selengkapnya <span class="fa fa-plus-square fa-lg" aria-hidden="true"></span></a></div>
+              </div>
+          </div>
+          <div class="col-lg-6">
+            <h3>Koleksi Terbaru</h3>
             <p>The Modern Business template by Start Bootstrap includes:</p>
             <ul>
                 <li><strong>Bootstrap v4</strong>
@@ -139,13 +115,84 @@
                 <li>Unstyled page elements for easy customization</li>
             </ul>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>
-        </div>
-        <div class="col-lg-6">
-            <img class="img-fluid rounded" src="http://placehold.it/700x450" alt="">
-        </div>
-    </div>
-    <!-- /.row -->
-
+            <div class="row">
+                <div class="col-lg-12 text-right"><a href="#" class="text-primary">Selengkapnya <span class="fa fa-plus-square fa-lg" aria-hidden="true"></span></a></div>
+            </div>
+          </div>
+      </div>
+      <!-- /.row -->
+</section>
+<hr>
+<section>
+  <!-- Marketing Icons Section -->
+      <div class="row">
+          <div class="col-lg-4 mb-4">
+              <div class="card h-100">
+                  <h4 class="card-header">Info Kegiatan</h4>
+                  <div class="card-block">
+                      <ul class="list-unstyled">
+                        <li class="pb-4">
+                          <div class="row">
+                            <div class="col-lg-4">
+                                <div class="card h-100">
+                                  <h4 class="card-header">OKT</h4>
+                                  <div class="card-block">
+                                      <h1 class="card-text">17</h1>
+                                  </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-8">
+                              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                            </div>
+                          </div>
+                        </li>
+                        <li>
+                          <div class="row">
+                            <div class="col-lg-4">
+                                <div class="card h-100">
+                                  <h4 class="card-header">OKT</h4>
+                                  <div class="card-block">
+                                      <h1 class="card-text">17</h1>
+                                  </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-8">
+                              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                            </div>
+                          </div>
+                        </li>
+                      </ul>
+                  </div>
+                  <div class="card-footer text-right">
+                    <a href="#" class="text-primary">Selengkapnya <span class="fa fa-plus-square fa-lg" aria-hidden="true"></span></a>
+                  </div>
+              </div>
+          </div>
+          <div class="col-lg-4 mb-4">
+              <div class="card h-100">
+                  <h4 class="card-header">Berita</h4>
+                  <div class="card-block">
+                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ipsam eos, nam perspiciatis natus commodi similique totam consectetur praesentium molestiae atque exercitationem ut consequuntur, sed eveniet, magni nostrum sint fuga.</p>
+                  </div>
+                  <div class="card-footer text-right">
+                      <a href="#" class="text-primary">Selengkapnya <span class="fa fa-plus-square fa-lg" aria-hidden="true"></span></a>
+                  </div>
+              </div>
+          </div>
+          <div class="col-lg-4 mb-4">
+              <div class="card h-100">
+                  <h4 class="card-header">Pengumuman</h4>
+                  <div class="card-block">
+                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                  </div>
+                  <div class="card-footer text-right">
+                      <a href="#" class="text-primary">Selengkapnya <span class="fa fa-plus-square fa-lg" aria-hidden="true"></span></a>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <!-- /.row -->
+</section>
     <hr>
 
     <!-- Call to Action Section -->
